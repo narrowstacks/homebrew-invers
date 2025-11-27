ac722e3103b028d84e28c2f400c827a67860ca93
narrowstacks <aaron@affords.art>
2025-11-27 00:18:38 -0800

    Update invers to 0.2.3

--- Formula/invers.rb
+++ Formula/invers.rb
@@ -1,25 +1,25 @@
 class Invers < Formula
   desc "Professional-grade film negative to positive conversion tool"
   homepage "https://github.com/narrowstacks/invers"
-  version "0.2.2"
+  version "0.2.3"
   license any_of: ["MIT"]
 
   on_macos do
     on_intel do
       url "https://github.com/narrowstacks/invers/releases/download/v#{version}/invers-x86_64-apple-darwin.tar.gz"
-      sha256 "4abf53abb62c81467d86a21af608150cb5dfb408e70b1acff1b8d3d279796553"
+      sha256 "9e8a6e309f6a4d2accb708f88936d5024f08f00a2e48058bc8e44b7f9c5d9ca2"
     end
 
     on_arm do
       url "https://github.com/narrowstacks/invers/releases/download/v#{version}/invers-aarch64-apple-darwin.tar.gz"
-      sha256 "fa962865b6784a6dc2203d1435593d3791d08008d5b46fdece4fb11277882b0e"
+      sha256 "aaffe25b83f29e679a21b64c984b2064ba2839194434cfedb3b6ab2ea767651b"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/narrowstacks/invers/releases/download/v#{version}/invers-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0d91ce46e037e13630dbdeffdac36669c97d875bcd3db3568d18fe7af1657ef0"
+      sha256 "de829a9e6c963e94ccfeaae69374c30130332e198ac5bf2b9610ccc62f9e65e0"
     end
   end
 
